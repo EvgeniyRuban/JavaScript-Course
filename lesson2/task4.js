851b42106df3996d674c4ef0af5e7ffd76ae6b1e
EvgeniyRuban <eugene_1997@inbox.ru>
2021-12-24 03:58:09 +0600

    Complited lesson2 tasks from 1 to 5.

---
+++ lesson2/task4.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/**
+ * @param {number} arg1 
+ * @param {number} arg2 
+ */
+function plus(arg1, arg2)
+{
+    return arg1 + arg2;
+}
+
+/**
+ * @param {number} arg1 
+ * @param {number} arg2 
+ */
+function minus(arg1, arg2)
+{
+    return arg1 - arg2;
+}
+
+/**
+ * @param {number} arg1 
+ * @param {number} arg2 
+ */
+function mult(arg1, arg2)
+{
+    return arg1 * arg2;
+}
+
+/**
+ * @param {number} arg1 
+ * @param {number} arg2 
+ */
+function divis(arg1, arg2)
+{
+    return arg1 / arg2;
+}
